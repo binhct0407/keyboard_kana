6ca1bd035d32fc19273b135b28e2424fc79ac364
lannv <lannv@runsystem.net>
2018-03-29 17:44:01 +0700

    update

---
+++ app/src/main/java/com/nvstudio/input/MainActivity.java
@@ -0,0 +1,20 @@
+package com.nvstudio.input;
+
+import android.inputmethodservice.Keyboard;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("mozc");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Keyboard
+    }
+
+}
